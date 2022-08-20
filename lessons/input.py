3bafbf897223be0e4da8626e74c876e2ccbdd4bd
Rebecca <rebhen@unc.edu>
2022-08-20 18:17:53 -0400

    Input Program + Age

--- lessons/input.py
+++ lessons/input.py
@@ -4,3 +4,9 @@ user_name: str = input("What is your name? ")
 
 print("Hello, " + user_name + ", good morning!")
 print("You are awesome, " + user_name)
+
+age: int = 4
+age = age + 1
+print(age)
+
+age = 18
