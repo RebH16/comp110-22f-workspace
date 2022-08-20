8877de28adfd36b9e4ab44ac54bb61faa965bb02
Rebecca <rebhen@unc.edu>
2022-08-20 17:16:51 -0400

    Edited "Hello, world." Program

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,5 @@
 """This is my first program in Comp 110 and it is a 'Hello, world.' program."""
 
-__author__ = 730568515
+__author__ = "730568515"
 
 print("Hello, world. My name is Rebecca and I'm excited to learn more!")
