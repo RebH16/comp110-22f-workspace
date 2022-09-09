1b0af6534c5ccde29a9022956039e1389cf6d657
Rebecca <rebhen@unc.edu>
2022-09-09 14:48:29 -0400

    Finished ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""This code will run one-shot wordl as an intermediary lesson befroe making full wordle!"""
+"""This code will run one-shot wordl as an intermediary lesson before making full wordle!"""
 
 __author__ = "730568515"
 # while index < secret_len:
@@ -13,27 +13,30 @@ secret_len: int = len(secret_word)
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-i = 0
+i: int = 0
 user_guess: str = input(f"What is your {secret_len}-letter guess? ")
 emoji: str = ""
-guessed_character_existance = False
-alternate_indices = 0
+guessed_character_existance: bool = False
+alternate_indice: int = 0
 
 while len(user_guess) != secret_len:
     user_guess = input(f"That was not {secret_len} letters! Try again: ")
 
 """Emoji set-up"""
 while i < secret_len:
+    alternate_indices = 0
+    guessed_character_existance = False
     if user_guess[i] == secret_word[i]:
         emoji += GREEN_BOX
-    while guessed_character_existance is not True and alternate_indices < secret_len and user_guess[i] != secret_word[i]:
-        if secret_word[alternate_indices] == user_guess[i]:
-            guessed_character_existance = True
-            emoji += YELLOW_BOX
-        else:
-            alternate_indices += 1
-    if user_guess[i] != secret_word[i] == False and guessed_character_existance == False:
-        emoji += WHITE_BOX
+    else:
+        while guessed_character_existance is not True and alternate_indices < secret_len:
+            if secret_word[alternate_indices] == user_guess[i]:
+                guessed_character_existance = True
+                emoji += YELLOW_BOX
+            else:
+                alternate_indices += 1
+        if not guessed_character_existance:
+            emoji += WHITE_BOX
     i += 1
 
 print(emoji)
