1b925d470a410d8666dd14098f4660d502d95d89
Rebecca <rebhen@unc.edu>
2022-09-08 23:47:33 -0400

    Perfect minus yellow

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,7 +1,7 @@
-"""This code will run one-shot wordl as an intermediary lesson befroe making full wordl"""
+"""This code will run one-shot wordl as an intermediary lesson befroe making full wordle!"""
 
 __author__ = "730568515"
-#while index < secret_len:
+# while index < secret_len:
 #    if user_guess[index] == secret_word[index]:
 #        emoji += GREEN_BOX
 #    else:
@@ -9,32 +9,39 @@ __author__ = "730568515"
 #    index += 1
 """Variable Definitions"""
 secret_word: str = "python"
-secret_len: str = len(secret_word)
+secret_len: int = len(secret_word)
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 i = 0
-user_guess: str = input (f"What is your {secret_len}-letter guess? ")
+user_guess: str = input(f"What is your {secret_len}-letter guess? ")
 emoji: str = ""
 guessed_character_existance = False
 alternate_indices = 0
 
 while len(user_guess) != secret_len:
-    user_guess = input (f"That was not {secret_len} letters! Try again: ")
+    user_guess = input(f"That was not {secret_len} letters! Try again: ")
 
 """Emoji set-up"""
+# while i < secret_len:
+#    if user_guess[i] == secret_word[i]:
+#        emoji += GREEN_BOX
+#    while guessed_character_existance is not True and alternate_indices < secret_len:
+#        if secret_word[alternate_indices] == user_guess[i]:
+#            guessed_character_existance = True
+#            emoji += YELLOW_BOX
+#        else:
+#            alternate_indices += 1
+#    i += 1
+#    if guessed_character_existance == False:
+#        emoji += WHITE_BOX
+
 while i < secret_len:
     if user_guess[i] == secret_word[i]:
         emoji += GREEN_BOX
-    while guessed_character_existance is not True and alternate_indices < secret_len:
-        if secret_word[alternate_indices] == user_guess[i]:
-            guessed_character_existance = True
-            emoji += YELLOW_BOX
-        else:
-            alternate_indices += 1
-    i += 1
-    if guessed_character_existance == False:
+    else:
         emoji += WHITE_BOX
+    i += 1
 
 print(emoji)
 
