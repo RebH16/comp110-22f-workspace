8cda94bdb0ccd4e0a86f53e6e4d8ae0ea023076a
Rebecca <rebhen@unc.edu>
2022-09-14 20:35:37 -0400

    First Final ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,10 +2,14 @@
 
 __author__ = "730568515"
 
+from mimetypes import guess_all_extensions
+
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-secret = "Codes"
+secret: str = "codes"
+
 #Part 1
 def contains_char(searched_string, searched_character) -> bool: 
     """Searches for individual character in full string."""
@@ -19,8 +23,9 @@ def contains_char(searched_string, searched_character) -> bool:
         i += 1
     return character_existance
 
+#Part 2
 def emojified(guess, secret) -> str:
-    "Checks for letter correctness and outputs a yellow, white, or green emoji block"
+    "Checks for letter correctness and outputs a yellow, white, or green emoji block."
     assert len(guess) == len (secret)
     i: int = 0
     emoji = ""
@@ -33,5 +38,30 @@ def emojified(guess, secret) -> str:
             emoji += WHITE_BOX
         i += 1
     return emoji
-#def input_guess(expected_length) -> bool:
-#    input(f"Enter a {len(secret)} character word: ")
+
+#Part 3
+def input_guess(expected_length) -> str:
+    """Ensuring a logical guess as the input."""
+    user_guess = input(f"Enter a {expected_length} character word: ")
+    while len(user_guess) != expected_length:
+        user_guess = input(f"That was not {expected_length} chars! Try again: ") 
+    return user_guess
+
+#Part 4
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    turn_number : int = 1
+    guess: str = ""
+    while turn_number <= 6 and secret != guess:
+        print(f"=== Turn {turn_number}/6 ===")
+        guess = input_guess(len(secret))
+        print(emojified(guess, secret))
+        turn_number += 1 
+    if secret == guess:
+        print(f"You won in {turn_number - 1}/6 turns!")
+    if secret != guess and turn_number > 6:
+        print("X/6 - Sorry, try again tomorrow!")
+
+#Allows the code to run as a module
+if __name__ == "__main__":
+    main()
