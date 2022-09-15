94d628ed7c339c70c174978b53ac8557f7f99c84
Rebecca <rebhen@unc.edu>
2022-09-14 22:44:34 -0400

    Final ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,16 +2,14 @@
 
 __author__ = "730568515"
 
-from mimetypes import guess_all_extensions
-
-
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 secret: str = "codes"
 
-#Part 1
-def contains_char(searched_string, searched_character) -> bool: 
+
+# Part 1
+def contains_char(searched_string: str, searched_character: str) -> bool: 
     """Searches for individual character in full string."""
     assert len(searched_character) == 1
     i = 0
@@ -23,10 +21,11 @@ def contains_char(searched_string, searched_character) -> bool:
         i += 1
     return character_existance
 
-#Part 2
-def emojified(guess, secret) -> str:
-    "Checks for letter correctness and outputs a yellow, white, or green emoji block."
-    assert len(guess) == len (secret)
+
+# Part 2
+def emojified(guess: str, secret: str) -> str:
+    """Checks for letter correctness and outputs a yellow, white, or green emoji block."""
+    assert len(guess) == len(secret)
     i: int = 0
     emoji = ""
     while i < len(secret):
@@ -39,18 +38,20 @@ def emojified(guess, secret) -> str:
         i += 1
     return emoji
 
-#Part 3
-def input_guess(expected_length) -> str:
+
+# Part 3
+def input_guess(expected_length: int) -> str:
     """Ensuring a logical guess as the input."""
     user_guess = input(f"Enter a {expected_length} character word: ")
     while len(user_guess) != expected_length:
-        user_guess = input(f"That was not {expected_length} chars! Try again: ") 
+        user_guess = input(f"That wasn't {expected_length} chars! Try again: ") 
     return user_guess
 
-#Part 4
+
+# Part 4
 def main() -> None:
     """The entrypoint of the program and main game loop."""
-    turn_number : int = 1
+    turn_number: int = 1
     guess: str = ""
     while turn_number <= 6 and secret != guess:
         print(f"=== Turn {turn_number}/6 ===")
@@ -62,6 +63,7 @@ def main() -> None:
     if secret != guess and turn_number > 6:
         print("X/6 - Sorry, try again tomorrow!")
 
-#Allows the code to run as a module
+
+# Allows the code to run as a module
 if __name__ == "__main__":
     main()
