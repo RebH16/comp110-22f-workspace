07ce404371c20ea5a2cfbbe9f92eb273a4f8297e
Rebecca <rebhen@unc.edu>
2022-09-19 15:51:54 -0400

    First Draft ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -4,12 +4,33 @@ __author__ = "730568515"
 
 def all(num_list: list[int], num: int) -> bool:
     i: int = 0
-    truth_check: int = 0
     while i <= len(num_list) - 1:
-        if num_list[i] == num:
-            truth_check += 1    
+        if num_list[i] != num: 
+            return False
         i += 1
-    if truth_check == len(num_list) - 1:
-        return True
     else:
-        return False
+        return True
+
+def max(input: list[int]) -> int:
+    i: int = 0
+    max_value: int = 0
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    while i < len(input):
+        if max_value < input[i]:
+            max_value = input[i]
+        i += 1
+    return max_value
+
+def is_equal(list_one: list[int], list_two: list[int]) -> bool:
+    i: int = 0
+    true_condition: int = 0
+    while i < len(list_one):
+        if list_one[i] == list_two[i]:
+            true_condition += 1
+        i += 1
+    if true_condition == len(list_one):
+        true_condition = True
+    else:
+        true_condition = False
+    return true_condition
