321489e8b6039f21c17d7046167e0e9de92452d8
Rebecca <rebhen@unc.edu>
2022-09-19 00:44:14 -0400

    ex04 Part 1 Partial Completion

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,3 +2,14 @@
 
 __author__ = "730568515"
 
+def all(num_list: list[int], num: int) -> bool:
+    i: int = 0
+    truth_check: int = 0
+    while i <= len(num_list) - 1:
+        if num_list[i] == num:
+            truth_check += 1    
+        i += 1
+    if truth_check == len(num_list) - 1:
+        return True
+    else:
+        return False
