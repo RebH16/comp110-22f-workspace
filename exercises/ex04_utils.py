a2357f5d082348083d595db4653dca632001730f
Rebecca <rebhen@unc.edu>
2022-09-19 16:14:39 -0400

    ex04 final

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,8 +2,12 @@
 
 __author__ = "730568515"
 
+
 def all(num_list: list[int], num: int) -> bool:
+    """Shows if the numbers in a list are all equal to a specific number."""
     i: int = 0
+    if len(num_list) == 0:
+        return False
     while i <= len(num_list) - 1:
         if num_list[i] != num: 
             return False
@@ -11,9 +15,11 @@ def all(num_list: list[int], num: int) -> bool:
     else:
         return True
 
+
 def max(input: list[int]) -> int:
+    """Finds the largest number in a list."""
     i: int = 0
-    max_value: int = 0
+    max_value: int = input[0]
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     while i < len(input):
@@ -22,15 +28,20 @@ def max(input: list[int]) -> int:
         i += 1
     return max_value
 
+
 def is_equal(list_one: list[int], list_two: list[int]) -> bool:
+    """Assess if two lists are exactly the same."""
     i: int = 0
     true_condition: int = 0
+    bool_condition: bool = True
+    if len(list_one) != len(list_two):
+        return False
     while i < len(list_one):
         if list_one[i] == list_two[i]:
             true_condition += 1
         i += 1
     if true_condition == len(list_one):
-        true_condition = True
+        bool_condition = True
     else:
-        true_condition = False
-    return true_condition
+        bool_condition = False
+    return bool_condition
