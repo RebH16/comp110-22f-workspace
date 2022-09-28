a3de5186e9a507e67fe290c712876c7a67a78d3e
Rebecca <rebhen@unc.edu>
2022-09-28 16:45:03 -0400

    Finished ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,4 +1,8 @@
+"""This module contains 3 functions that adapt lists."""
+
+
 __author__ = "730568515"
+
 
 def only_evens(list: list[int]) -> list:
     """Takes in a list and makes a list with only even numbers from that initial list."""
@@ -10,14 +14,33 @@ def only_evens(list: list[int]) -> list:
         i += 1
     return even_list
 
+
 def concat(first_list: list[int], second_list: list[int]) -> list:
-    """Combines two lists so that the second list is added to the first list"""
-    first_list.append(second_list)
-    return first_list
+    """Combines two lists so that the second list is added to the first list."""
+    combined_list: list = []
+    i: int = 0
+    while i < len(first_list):
+        combined_list.append(first_list[i])
+        i += 1
+    i = 0
+    while i < len(second_list):
+        combined_list.append(second_list[i])
+        i += 1
+    return combined_list
+
 
 def sub(a_list: list[int], start_index: int, end_index: int) -> list:
-    """Creates a new list from a portion of the original list"""
-    list_start = a_list.pop[:end_index]
-    list_end = a_list.pop[start_index:]
-    full_list = list_start + list_end
-    return full_list
+    """Creates a new list from a portion of the original list."""
+    i: int = start_index
+    y: int = end_index
+    new_list: list = []
+    if i < 0:
+        i = 0
+    if i > len(a_list) or y <= 0:
+        return []
+    if y >= len(a_list):
+        y = len(a_list)
+    while i < y:
+        new_list.append(a_list[i])
+        i += 1
+    return new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,34 +1,79 @@
+"""This module contains tests for the utils module."""
+
+
 __author__ = "730568515"
 
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
 """Tests for only_evens."""
-from exercises.ex05.utils import only_evens
+
 
 def test_only_evens_empty() -> None:
+    """Tests for when the list is empty."""
     xs: list[int] = []
     assert only_evens(xs) == []
 
+
 def test_only_evens_only_evens() -> None:
+    """Tests for when the list only contains evens."""
     xs: list[int] = [2, 4, 8, 10, 12]
     assert only_evens(xs) == xs
 
+
 def test_only_evens_only_odds() -> None:
+    """Tests for when the list only contains odds."""
     xs: list[int] = [3, 5, 7, 13, 15]
     assert only_evens(xs) == []
 
+
 """Tests for concat."""
-from exercises.ex05.utils import concat
+
 
 def test_concat_empty() -> None:
+    """Tests for when the lists are empty."""
     xs: list = []
     xl: list = []
-    assert concat(xs, xl) == [[]]
+    assert concat(xs, xl) == []
+
 
 def test_concat_empty_full() -> None:
+    """Tests for when one list is empty."""
     xs: list = []
     xl: list = [1, 2, 3, 4, 5]
-    assert concat(xs, xl) == [xl]
+    assert concat(xs, xl) == xl
+
 
 def test_concat_full_full() -> None:
+    """Tests for when both lists have contents."""
     xs: list = [1, 2, 3]
     xl: list = [4, 5, 6]
-    assert concat(xs, xl) == [xs + [xl]]
+    assert concat(xs, xl) == xs + xl
+
+
+"""Tests for sub."""
+
+
+def test_sub_empty() -> None:
+    """Tests for when the list is empty."""
+    list: list = []
+    xs: int = 0
+    xl: int = 0
+    assert sub(list, xs, xl) == []
+
+
+def test_sub_negative_start() -> None:
+    """Tests for when the start index is negative."""
+    list: list = [0, 1, 2, 3, 4]
+    xs: int = -2
+    xl: int = 0
+    assert sub(list, xs, xl) == []
+
+
+def test_sub_big_end() -> None:
+    """Tests for when the end index is larger than the list length."""
+    list: list = [0, 1, 2, 3, 4]
+    xs: int = 0
+    xl: int = 10
+    assert sub(list, xs, xl) == [0, 1, 2, 3, 4]
