c7f906d2b890ccea3ef00f12841ee602c2b7b7d1
Rebecca Henriques <rebhen@unc.edu>
2022-11-01 18:18:41 -0400

    Mostly done

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -5,8 +5,9 @@ __author__ = "730568515"
 # Define your functions below
 from csv import DictReader
 
+
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
-    """Read the rows of a CSV into a 'table'"""
+    """Read the rows of a CSV into a 'table'."""
     result: list[dict[str, str]] = []
     file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
@@ -14,6 +15,7 @@ def read_csv_rows(filename: str) -> list[dict[str, str]]:
         result.append(row)
     file_handle.close
     return result
+
 
 def column_values(table: list[dict[str, str]], column: str) -> list[str]:
     """Produces a list[str] of all values in a signle column.""" 
@@ -23,6 +25,7 @@ def column_values(table: list[dict[str, str]], column: str) -> list[str]:
         result.append(item)
     return result
 
+
 def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
     """Transform a row-oriented table to a column-oriented table."""
     result: dict[str, list[str]] = {}
@@ -31,33 +34,42 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
         result[column] = column_values(row_table, column)
     return result
 
-def head(table: dict[str, list[str]], row_count: int) -> dict[str, list[str]]:
+
+def head(table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """Reads the top of a table."""
     result: dict[str, list[str]] = {}
     for column in table:
         first_values: list = []
-        for item in column:
-            first_values.append(item)
-        result[row_count] = first_values
+        i: int = 0
+        for i in range(N):
+            first_values.append(table[column][i])
+        result[column] = first_values
     return result
 
+
 def select(table: dict[str, list[str]], column_copy: list[str]) -> dict[str, list[str]]:
+    """Looks at select columns of data."""
     result: dict[str, list[str]] = {}
     for column in column_copy:
         result[column] = table[column]
     return result
 
+
 def concat(table_one: dict[str, list[str]], table_two: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Combines two tables."""
     result: dict[str, list[str]] = {}
     for column in table_one:
         result[column] = table_one[column]
     for column in table_two:
-        if table_two[column] in table_one:
+        if column in table_one:
             table_one[column] += table_two[column]
         else:
             table_one[column] = table_two[column]
     return result
         
+
 def count(list: list[str]) -> dict[str, int]:
+    """Counts items."""
     result: dict[str, int] = {}
     i: int = 0
     while i < len(list):
